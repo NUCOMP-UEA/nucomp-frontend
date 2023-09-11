effa45ef308114a5a04a3c49d658c8a3663436ef
Gabi196 <gabriellarabimonteiro@gmail.com>
2023-09-11 17:41:16 -0400

    feat: add menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,69 @@
-import styles from './Header.module.css'
-import {ReactComponent as HamburgerIcon} from '../assets/hamburger.svg'
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { SidebarData } from "./SideBar/SideBarData";
+import SubMenu from "./SideBar/SubMenu";
+
+const Nav = styled.div`
+  background: #fff;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+ 
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+ 
+const SidebarNav = styled.nav`
+  background: #0f7c94;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+  transition: 350ms;
+  z-index: 10;
+`;
+ 
+const SidebarWrap = styled.div`
+  width: 100%;
+`;
 
 export function Header() {
-    return (
-        <header className={styles.header}>
-            <HamburgerIcon />
-            <div className={styles.hcontent}>
-                NUCOMP | Núcleo de Computação da UEA
-            </div>
-        </header>
-    );
+  const [sidebar, setSidebar] = useState(true);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <FiMenu onClick={showSidebar}/>
+        <div className={styles.hcontent}>
+          NUCOMP | Núcleo de Computação da UEA
+        </div>
+      </header>
+      <SidebarNav sidebar={sidebar}>
+        <SidebarWrap>
+          <NavIcon to="#">
+            <AiOutlineClose onClick={showSidebar} color="#e1e9fc"/>
+          </NavIcon>
+          {SidebarData.map((item, index) => {
+            return <SubMenu item={item} key={index} />;
+          })}
+        </SidebarWrap>
+      </SidebarNav>
+    </>
+  );
 }
---
+++ src/components/SideBar/SideBarData.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import {FaRegBuilding} from "react-icons/fa"
+import { AiOutlineClose } from "react-icons/ai";
+import {BiBookOpen} from "react-icons/bi";
+import { HiOutlineNewspaper, HiOutlineIdentification} from "react-icons/hi";
+import  { PiGraduationCap, PiCalculator} from "react-icons/pi";
+import { BsTelephone } from "react-icons/bs";
+
+export const SidebarData = [
+  {
+    title: "Institucional",
+    path: "/home",
+    icon:  <FaRegBuilding />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+    subNav: [
+      {
+        title: "Sobre",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Localização",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Corpo docente",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Formas de ingresso",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Publicações",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    title: "Pesquisa",
+    path: "/home",
+    icon:  <BiBookOpen />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+    subNav: [
+      {
+        title: "LSE",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "LSI",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Samsung OCEAN",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "FEMTOLAB",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "ThinkTED",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "LUDUS Lab",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Academia STEM",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    title: "Notícias",
+    path: "/home",
+    icon:  <HiOutlineNewspaper />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />
+  },
+  {
+    title: "Estágio",
+    path: "/home",
+    icon:  <HiOutlineIdentification />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+    subNav: [
+      {
+        title: "Orientação",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Documentos",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    title: "Cursos",
+    path: "/home",
+    icon:  <PiGraduationCap />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+    subNav: [
+      {
+        title: "Graduações",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    title: "Serviços",
+    path: "/home",
+    icon:  <PiCalculator />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+    subNav: [
+      {
+        title: "Calculadora de Horas complementares",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+      {
+        title: "Match Aluno x Orientador",
+        path: "/about-us/aim",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    title: "Fale conosco",
+    path: "/home",
+    icon:  <BsTelephone />,
+    iconClosed: <FiChevronDown />,
+    iconOpened: <FiChevronUp />,
+  },
+
+];
