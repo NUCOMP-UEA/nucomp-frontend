84ee4915efeae1978eb61ce2891579ed2bccb6c8
femto_security_front <software2.ssac@gmail.com>
2023-09-14 18:00:14 -0400

    feat: improve additional hour search

--- src/pages/Calculator/components/AdditionalHoursTable/AdditionalSearch/index.jsx
+++ src/pages/Calculator/components/AdditionalHoursTable/AdditionalSearch/index.jsx
@@ -1,11 +1,24 @@
-import * as Icon from '../../icons';
-import { Container } from './styles';
+import { useRef } from "react";
+import * as Icon from "../../icons";
+import { Container } from "./styles";
 
 export const AdditionalSearch = () => {
+  const searchRef = useRef("");
+
+  function onSearch(event) {
+    event.preventDefault();
+    if (searchRef.current) {
+      console.log(searchRef.current);
+    }
+  }
   return (
     <Container>
-      <input type="text" className="input-search" />
-      <button className="button-search" onClick={() => console.log('search')}>
+      <input
+        type="text"
+        className="input-search"
+        onChange={(event) => (searchRef.current = event.target.value)}
+      />
+      <button className="button-search" onClick={onSearch}>
         <Icon.Search />
       </button>
     </Container>
