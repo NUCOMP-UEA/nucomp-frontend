ce6e3091918df20ffbd48ceb79c2d77ffc617c42
femto_security_front <software2.ssac@gmail.com>
2023-09-14 20:13:36 -0400

    refactor: remove stepper header folder

--- src/pages/Calculator/components/Stepper/index.jsx
+++ src/pages/Calculator/components/Stepper/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import { StepperHeader } from './StepperHeader';
-import { Step1 } from './Steps/Step1';
-import { Step2 } from './Steps/Step2';
-import { Step3 } from './Steps/Step3';
-import { Container } from './styles';
+import { Step1 } from "./Steps/Step1";
+import { Step2 } from "./Steps/Step2";
+import { Step3 } from "./Steps/Step3";
+import { Container, StepperHeader, StepperOption } from "./styles";
+import * as Icon from "../icons";
 
 export const Stepper = (props) => {
   function onNextStep1(payload) {
@@ -12,7 +12,17 @@ export const Stepper = (props) => {
   }
   return (
     <Container>
-      <StepperHeader step={props.step} />
+      <StepperHeader>
+        <StepperOption checked={props.step === 1}>
+          <Icon.User />
+        </StepperOption>
+        <StepperOption checked={props.step === 2}>
+          <Icon.Calculator />
+        </StepperOption>
+        <StepperOption checked={props.step === 3}>
+          <Icon.Check />
+        </StepperOption>
+      </StepperHeader>
       {props.step === 1 ? <Step1 onNext={onNextStep1} /> : null}
       {props.step === 2 ? <Step2 onNext={() => props.setStep(3)} /> : null}
       {props.step === 3 ? <Step3 /> : null}
